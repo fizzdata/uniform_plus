17604ccc322c889b5a9680e8ea10370c448d2a46
Yoely@peneh <yoely@peneh.com>
2025-05-08 14:26:31 -0400

    register middelware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,3 +16,10 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+
+    $middleware->alias([
+        'auth.shopify' => \App\Http\Middleware\ShopifyAuthMiddleware::class,
+
+    ]);
+
