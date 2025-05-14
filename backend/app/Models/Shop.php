3bd34471858ede1d6dc80e60d422910ba895c173
Yoely@peneh <yoely@peneh.com>
2025-05-13 22:30:39 -0400

    added shop to fillabel

--- backend/app/Models/Shop.php
+++ backend/app/Models/Shop.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Shop extends Model
 {
-    //
+    protected $fillable = ['shop_domain', 'access_token'];
+
+    public function getShopDomainAttribute($value)
+    {
+        return strtolower($value);
+    }
+
+    public function setShopDomainAttribute($value)
+    {
+        $this->attributes['shop_domain'] = strtolower($value);
+    }
+
+
 }
+
+
