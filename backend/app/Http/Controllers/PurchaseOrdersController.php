ac6a3cef8a14e16694fc407de982ac2a2a103761
Yoely@peneh <yoely@peneh.com>
2025-05-26 16:13:10 -0400

    fixed adjust inventory shopify'

--- backend/app/Http/Controllers/PurchaseOrdersController.php
+++ backend/app/Http/Controllers/PurchaseOrdersController.php
@@ -85,8 +85,8 @@ public function receive(Request $request)
         
             
         $adjustmentResponse = Http::withHeaders([
-            'X-Shopify-Access-Token' => $request->access_token,
-        ])->post("https://{$request->shop}/admin/api/2024-10/inventory_levels/set.json", [
+            'X-Shopify-Access-Token' => $request->shop->access_token,
+        ])->post("https://{$request->shop->shop_domain}/admin/api/2024-10/inventory_levels/adjust.json", [
             'location_id' => $request->location_id,          // From step 1
             'inventory_item_id' => $order->shopify_product_id,   
             'available' => $order->quantity_received
