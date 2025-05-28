eb2fb3ad307724cd80e28199a896abc08f59acd7
yahya1044 <yahya.ibrahim1044@gmail.com>
2025-05-28 18:44:29 +0500

    purchase order edit & delete api added

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,8 +6,14 @@ import router from "./router";
 import "@shopify/polaris/build/esm/styles.css";
 import Vue3Toastify from "vue3-toastify";
 import "vue3-toastify/dist/index.css"; // Make sure Toastify styles are loaded
+import {
+  // Directives
+  vTooltip,
+} from "floating-vue";
+import "floating-vue/dist/style.css";
 
 const app = createApp(App); // ✅ Create the app instance first
+app.directive("tooltip", vTooltip);
 
 app.use(router); // ✅ Use router
 app.use(Vue3Toastify, {
