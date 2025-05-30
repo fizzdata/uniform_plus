7c49a865e127476e58f7fa27970ce2eebc08013c
Yoely@peneh <yoely@peneh.com>
2025-05-30 10:48:59 -0400

    added slack reporting

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Log;
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +17,11 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $this->reportable(function (Throwable $e) {
+          Log::channel('slack')->error($e->getMessage(),[
+              'file' => $e->getFile(),
+              'Line' => $e->getLine(),
+              'code' => $e->getCode(),
+          ]);
+      });
     })->create();
