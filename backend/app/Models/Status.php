a98e119559a5203d61cae6046a05f716d9b16a74
fizzdata <146122235+fizzdata@users.noreply.github.com>
2025-07-28 19:36:48 +0000

    added phone support hendeling

--- backend/app/Models/Status.php
+++ backend/app/Models/Status.php
@@ -23,44 +23,90 @@ class Status extends Model
          return isset($item['title']) && stripos($item['title'], 'logo') !== false;
      });
 
+    $has_phone_in_notes = isset($order['note']) && stripos($order['note'], 'phone') !== false;
     
-    $sourceKey = $source . '_' . ($financial === 'paid' ? 'paid' : 'unpaid')  . ($hasLogoItem ? '_logo' : '');
+    $sourceKey = $source . '_' . ($financial === 'paid' ? 'paid' : 'unpaid')  . ($hasLogoItem ? '_logo' : '') . ($has_phone_in_notes ? '_phone' : '');
+
 
 switch ($sourceKey) {
+    // POS
     case 'pos_paid':
         $statusName = 'Paid in Store';
+        break;
+    case 'pos_paid_phone':
+        $statusName = 'Paid in Store (Phone)';
+        break;
+    case 'pos_paid_logo':
+        $statusName = 'Paid in Store with Logo';
+        break;
+    case 'pos_paid_logo_phone':
+        $statusName = 'Paid in Store with Logo (Phone)';
         break;
     case 'pos_unpaid':
         $statusName = 'POS order Started';
         break;
+    case 'pos_unpaid_phone':
+        $statusName = 'POS order Started (Phone)';
+        break;
+    case 'pos_unpaid_logo':
+        $statusName = 'POS order Started with Logo';
+        break;
+    case 'pos_unpaid_logo_phone':
+        $statusName = 'POS order Started with Logo (Phone)';
+        break;
+
+    // WEB
     case 'web_paid':
         $statusName = 'Paid on Website';
+        break;
+    case 'web_paid_phone':
+        $statusName = 'Paid on Phone';
+        break;
+    case 'web_paid_logo':
+        $statusName = 'Paid on Website with Logo';
+        break;
+    case 'web_paid_logo_phone':
+        $statusName = 'Paid on Phone with Logo';
         break;
     case 'web_unpaid':
         $statusName = 'Web order Started';
         break;
-    case 'phone_paid':
-        $statusName = 'Paid on Phone';
-        break;
-    case 'phone_unpaid':
+    case 'web_unpaid_phone':
         $statusName = 'Phone order Started';
         break;
-    case 'shopify_draft_order_paid':
-        $statusName = 'Paid on Phone'; // Assuming same as phone order
+    case 'web_unpaid_logo':
+        $statusName = 'Web order Started with Logo';
         break;
-    case 'shopify_draft_order_unpaid':
-        $statusName = 'Draft Order';
+    case 'web_unpaid_logo_phone':
+        $statusName = 'Phone order Started with Logo';
+        break;
+
+    // SHOPIFY DRAFT ORDER
+    case 'shopify_draft_order_paid':
+        $statusName = 'Paid on Phone';
+        break;
+    case 'shopify_draft_order_paid_phone':
+        $statusName = 'Paid on Phone (Draft)';
         break;
     case 'shopify_draft_order_paid_logo':
         $statusName = 'Draft Order with Logo';
         break;
+    case 'shopify_draft_order_paid_logo_phone':
+        $statusName = 'Draft Order with Logo (Phone)';
+        break;
+    case 'shopify_draft_order_unpaid':
+        $statusName = 'Draft Order';
+        break;
+    case 'shopify_draft_order_unpaid_phone':
+        $statusName = 'Draft Order (Phone)';
+        break;
     case 'shopify_draft_order_unpaid_logo':
         $statusName = 'Draft Order with Logo';
-    case 'pos_paid_logo':
-        $statusName = 'Paid in Store with Logo';
         break;
-    case 'pos_unpaid_logo':
-        $statusName = 'POS order Started with Logo';
+    case 'shopify_draft_order_unpaid_logo_phone':
+        $statusName = 'Draft Order with Logo (Phone)';
+        break;
+
     default:
         $statusName = 'Custom order Started';
         break;
