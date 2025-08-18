eb187b0bf1375e7bb31b2e26eba9d6a64139365c
Yoely@peneh <yoely@peneh.com>
2025-08-18 15:08:58 -0400

    fixed updating status

--- backend/app/Models/Shopify.php
+++ backend/app/Models/Shopify.php
@@ -100,7 +100,7 @@ class Shopify extends Model
 public function get_orders($page_info = null) {
 
     $url = "https://{$this->shop_domain}/admin/api/2024-10/orders.json";
-    $limit = 20; // Default limit for pagination
+    $limit = 100; // Default limit for pagination
     $fields = 'id,name,customer,created_at,current_total_price,order_status_url,source_name,financial_status,fulfillment_status,note,shipping_lines,line_items';
 
     $params = [
